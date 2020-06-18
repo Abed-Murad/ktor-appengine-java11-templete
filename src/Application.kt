7507293c860d94867f6508e89b69570ac62528e2
Abed Murad <Abdallah.Murad@ProtonMail.com>
2020-06-18 15:54:32 +0300

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,22 @@
+package com.am
+
+import io.ktor.application.*
+import io.ktor.http.ContentType
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.jetty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("hello world:v1", contentType = ContentType.Text.Plain)
+        }
+    }
+
+}
+
